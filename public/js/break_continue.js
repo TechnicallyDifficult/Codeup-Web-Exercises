bb63656a11ac43623cec0037a142ba9253dd3517
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-02-15 13:31:32 -0600

    Adds latest exercise.

---
+++ public/js/break_continue.js
@@ -0,0 +1,17 @@
+"use strict"
+
+var skip = 0;
+
+while (skip % 2 != 1) {
+    skip = Math.floor((Math.random()*50)+1);
+}
+
+console.log("Random odd number to skip is: " + skip);
+
+for (var i = 1; i < 50; i += 2) {
+    if (i == skip) {
+        console.log("Yikes! Skipping number: " + skip);
+        continue;
+    }
+    console.log("Here is an odd number: " + i);
+}
