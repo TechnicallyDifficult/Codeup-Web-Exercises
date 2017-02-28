9e0b5447ad3a790b5ec4d3249a3c42c65de806e5
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-02-28 11:29:53 -0600

    Adds rest of exercise 4.5.1

---
+++ public/js/ajax_store.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    'use strict';
+    const td = '</td><td>';
+
+    function renderTable() {
+        $('#insertProducts').html('');
+        $.get('./data/inventory.json').done(function (data) {
+            data.forEach(function (element, index, array) {
+                $('#insertProducts').append('<tr><td>' + element.title + td + element.quantity + td + element.price + td + element.categories.join(', ') + '</td></tr>');
+***REMOVED***);
+        });
+    }
+    renderTable();
+    $('#refresh').click(renderTable);
+});
