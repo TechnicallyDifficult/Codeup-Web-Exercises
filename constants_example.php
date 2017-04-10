a6c0ec43557b2ba385e7f06ec47aa806cd49b164
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-10 11:04:02 -0500

    adds files relating to connecting to MySQL databases

---
+++ constants_example.php
@@ -0,0 +1,10 @@
+<?php
+
+// this information should be in a file on your machine named "constants.php"
+// assign the constants to the appropriate values
+// **DO NOT COMMIT THIS FILE**
+
+const DB_HOST;
+const DB_NAME;
+const DB_USER;
+const DB_PASS;
